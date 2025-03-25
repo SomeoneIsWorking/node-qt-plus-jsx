25f85c5b6d1dba87a4452fde5de187c7d46b051b
Barış Hamil <barishamil90@gmail.com>
2025-03-25 20:37:05 +0300

    remove dist from git

--- dist/signal.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Signal<T> {
-    private value;
-    private listeners;
-    constructor(initialValue: T);
-    get(): T;
-    emit(newValue: T): void;
-    connect(listener: (value: T) => void): this;
-    private notify;
-}
